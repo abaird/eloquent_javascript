976a34d430c2ef7aaa640f66978f116e4645dba0
Alan Baird <abaird@bairdsnet.net>
2016-03-21 16:40:02 -0500

    adding exercises from chapter 2

---
+++ 02/chess_board.js
@@ -0,0 +1,14 @@
+var board_size = 4;
+var black_space = '#';
+var white_space = ' ';
+for (var board_lines = 1; board_lines <= board_size; board_lines++){
+    start_char = (board_lines % 2 == 0) ? black_space : white_space;
+    board_spaces = '';
+    for (var i = 1; i <= board_size; i++) {
+        if (i % 2 !== 0)
+            board_spaces += start_char;
+        else
+            board_spaces += start_char === white_space ? black_space : white_space;
+    }
+    console.log(board_spaces);
+}
